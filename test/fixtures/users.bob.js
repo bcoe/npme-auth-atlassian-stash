3c892b1d6441266a59d612b46b159ee64574ba24
Benjamin Coe <ben@npmjs.com>
2016-06-30 11:46:16 -0700

    chore(tests): add fixture for bob

---
+++ test/fixtures/users.bob.js
@@ -0,0 +1,20 @@
+module.exports = {
+    name: "bob",
+    emailAddress: "Bob@domain.com",
+    id: 1400,
+    displayName: "Bobby Bob",
+    active: true,
+    slug: "bob",
+    type: "NORMAL",
+    link: {
+        url: "/users/bob",
+        rel: "self"
+    },
+    links: {
+        self: [
+            {
+                href: "https://stash.doamin.com/users/bob"
+            }
+        ]
+    }
+};
